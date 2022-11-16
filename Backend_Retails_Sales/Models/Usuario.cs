c16aa6bd8518ddf3d87ef1e27a54fc747b54d777
CerveraNicolas <emilio.cervera@lmvsoftware.com>
2022-11-16 20:26:13 -0300

    add model user

---
+++ Backend_Retails_Sales/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace Backend_Retails_Sales.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [MaxLength(200)]
+        [Required]
+        public string ID_INTERNO_ACCION { get; set; }
+    }
+}
