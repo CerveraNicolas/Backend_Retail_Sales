6c658a430ebead28d96a7511888725686231a4a6
Nicolás Serna <nicolito128@hotmail.com>
2022-11-17 05:38:41 -0300

    Update migrations

---
+++ Backend_Retails_Sales/Migrations/20221117083827_v0.3.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend_Retails_Sales.Migrations
+{
+    public partial class v03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HoraRegistro",
+                table: "Producto");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Alias",
+                table: "Producto",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Alias",
+                table: "Producto",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "HoraRegistro",
+                table: "Producto",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+        }
+    }
+}
